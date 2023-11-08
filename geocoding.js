5115f709e19eae674ae022d2510a005966c7ba9d
Maiu <maiouwenneuh@gmail.com>
2023-11-08 15:53:55 +0100

    Add speed calculation and address fetching

--- geocoding.js
+++ geocoding.js
@@ -5,13 +5,20 @@ let maxAge = document.querySelector('input[name="maxAge"]');
 let timeout = document.querySelector('input[name="timeout"]');
 let watchPos = document.querySelector('button:nth-of-type(2)');
 let stopPos = document.querySelector('button:nth-of-type(3)');
+let speedCalc = document.querySelector('input[name="speed"]');
+let divAdress = document.querySelector('.adress');
 
 let options={};
 let id;
 let previousPos;
 
 const succès = (pos) => {
-    console.log(pos);
+    if(previousPos !=undefined && (pos.coords.latitude - previousPos.coords.latitude > 0.01 || pos.coords.longitude - previousPos.coords.longitude > 0.01)){
+        fetchAdress(pos.coords.latitude, pos.coords.longitude);
+    }
+    else if(previousPos == undefined){
+        fetchAdress(pos.coords.latitude, pos.coords.longitude);
+    }
     let speed = 0;
     if (previousPos) {
         const distance = getDistanceFromLatLonInMeters(previousPos.coords.latitude, previousPos.coords.longitude, pos.coords.latitude, pos.coords.longitude);
@@ -26,7 +33,7 @@ const succès = (pos) => {
     <br> Altitude : ${pos.coords.altitude}
     <br> Précision altitude : ${pos.coords.altitudeAccuracy}
     <br> Direction (deg) : ${pos.coords.heading}
-    <br> Vitesse (m/s) : ${speed}`; 
+    <br> Vitesse (m/s) : ${speedCalc.checked ? speed : pos.coords.speed}`; 
 }
 const erreur = (err) => {
     console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -35,7 +42,7 @@ const erreur = (err) => {
 getPos.addEventListener('click', () => {
     defOptions();
     navigator.geolocation.getCurrentPosition(succès, erreur, options);
-    console.log(options);
+    //console.log(options);
 });
 
 
@@ -63,8 +70,8 @@ function defOptions(){
 }
 
 function getDistanceFromLatLonInMeters(lat1,lon1,lat2,lon2) {
-        const R = 6371000; // Radius of the earth in meters
-        const dLat = deg2rad(lat2-lat1);  // deg2rad below
+        const R = 6371000; 
+        const dLat = deg2rad(lat2-lat1);  
         const dLon = deg2rad(lon2-lon1); 
         const a = 
             Math.sin(dLat/2) * Math.sin(dLat/2) +
@@ -72,10 +79,21 @@ function getDistanceFromLatLonInMeters(lat1,lon1,lat2,lon2) {
             Math.sin(dLon/2) * Math.sin(dLon/2)
             ; 
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-        const d = R * c; // Distance in meters
+        const d = R * c;
         return d;
 }
 
 function deg2rad(deg) {
     return deg * (Math.PI/180)
+}
+
+function fetchAdress(latitude, longitude){
+    divAdress.innerHTML = "";
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${latitude}&lon=${longitude}`)
+        .then((resp)=>resp.json())
+        .then((resp)=>{
+            for (let elt in resp.features[0].properties.address){
+                divAdress.innerHTML += `<br>${elt}: ${resp.features[0].properties.address[elt]}`;
+            }
+    })
 }
