2f2fdce4ee24ec415faac9dc025fe614c0686719
Maiu <maiouwenneuh@gmail.com>
2023-11-08 12:16:12 +0100

    Add speed calculation to geocoding.js

--- geocoding.js
+++ geocoding.js
@@ -8,10 +8,17 @@ let stopPos = document.querySelector('button:nth-of-type(3)');
 
 let options={};
 let id;
-let previousPos
+let previousPos;
 
 const succès = (pos) => {
     console.log(pos);
+    let speed = 0;
+    if (previousPos) {
+        const distance = getDistanceFromLatLonInKm(previousPos.coords.latitude, previousPos.coords.longitude, pos.coords.latitude, pos.coords.longitude);
+        const timeDiff = (pos.timestamp - previousPos.timestamp) / 1000; // convert to seconds
+        speed = distance / timeDiff;
+    }
+    previousPos = pos;
     div.innerHTML = ` Date de la requête : ${new Date(pos.timestamp).toLocaleString("fr-FR")}
     <br> Précision : ${pos.coords.accuracy}
     <br> Latitude : ${pos.coords.latitude} 
@@ -19,7 +26,7 @@ const succès = (pos) => {
     <br> Altitude : ${pos.coords.altitude}
     <br> Précision altitude : ${pos.coords.altitudeAccuracy}
     <br> Direction (deg) : ${pos.coords.heading}
-    <br> Vitesse (m/s) : ${pos.coords.speed}`;
+    <br> Vitesse (m/s) : ${speed}`;
 }
 const erreur = (err) => {
     console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -55,4 +62,20 @@ function defOptions(){
     options.maximumAge = maxAge.value;
 }
 
-   
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2-lat1);  // deg2rad below
+    const dLon = deg2rad(lon2-lon1); 
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
